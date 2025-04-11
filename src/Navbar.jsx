d6bda0b9eb29b73abc2c96650d1e479ee2f8961b
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-11 16:27:15 -0400

    init Navbar component

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,18 @@
+import { FaBars } from "react-icons/fa"
+import { useGlobalContext } from "./context"
+
 function Navbar() {
+  const { openSidebar } = useGlobalContext()
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='nav-center'>
+        <h3 className='logo'>Matthew</h3>
+        <button className='toggle-btn' onClick={openSidebar}>
+          <FaBars />
+        </button>
+      </div>
+    </nav>
   )
 }
 export default Navbar
