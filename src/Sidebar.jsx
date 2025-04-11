ff4d78c664e7bb66683d90274ab05b476a8b458f
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-11 16:53:00 -0400

    Implement Sidebar component with dynamic links and close button

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,38 @@
+import { FaTimes } from "react-icons/fa"
+import sublinks from "./data"
+import { useGlobalContext } from "./context"
+
 function Sidebar() {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
   return (
-    <div>Sidebar</div>
+    <aside className='sidebar'>
+      <div className='sidebar-container'>
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map((sublink) => {
+            const { links, page, pageId } = sublink
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link
+                    return (
+                      <a href={url} key={id}>
+                        {icon}
+                        {label}
+                      </a>
+                    )
+                  })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </aside>
   )
 }
 export default Sidebar
