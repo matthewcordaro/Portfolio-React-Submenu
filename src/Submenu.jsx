ff697393306bf7258caf16f3302fc75b5d6cfb20
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-11 20:53:02 -0400

    Implement Submenu component with dynamic links and styling

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,6 +1,24 @@
+import sublinks from "./data"
+import { useGlobalContext } from "./context"
+
+
 function Submenu() {
+  const {pageId, setPageId} = useGlobalContext()
+  if (pageId === null) return
+  const {links} = sublinks.find((sublink => sublink.pageId === pageId))
+  console.log(links)
   return (
-    <div>Submenu</div>
+    <div className="submenu show-submenu" onMouseLeave={()=>setPageId(null)}>
+      <div className="submenu-container">
+        <div className="submenu-links">
+          {links.map((link)=>{
+            const {url, icon, label, id} = link 
+            return <a href={url} key={id}>{icon}{label}</a>
+          })}
+          
+        </div>
+      </div>
+    </div>
   )
 }
 export default Submenu
